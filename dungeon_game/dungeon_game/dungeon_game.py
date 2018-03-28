6b09757fdc766aa33efa3ae381bfddb6939fb77e
James Lockhart <james.lockhart117@gmail.com>
2018-03-28 21:03:43 +0300

    function draws map

--- dungeon_game/dungeon_game/dungeon_game.py
+++ dungeon_game/dungeon_game/dungeon_game.py
@@ -1,17 +1,30 @@
 import os
 import random
 
-CELLS = [(0,0), (1,0), (2,0), (3,0), (4,0),
-         (0,1), (1,1), (2,1), (3,1), (4,1),
-         (0,2), (1,2), (2,2), (3,2), (4,2),
-         (0,3), (1,3), (2,3), (3,3), (4,3),
-         (0,4), (1,4), (2,4), (3,4), (4,4)]
+# draw grid
+# pick random location for player
+# pick random location for exit door
+# pick random location for the monster
+# draw player in the grid
+# take input for movement
+# move player, unless invalid move (past edges of grid)
+# check for win/loss
+# clear screen and redraw grid
+
+CELLS = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0),
+         (0, 1), (1, 1), (2, 1), (3, 1), (4, 1),
+         (0, 2), (1, 2), (2, 2), (3, 2), (4, 2),
+         (0, 3), (1, 3), (2, 3), (3, 3), (4, 3),
+         (0, 4), (1, 4), (2, 4), (3, 4), (4, 4)]
+
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def get_location():
+
+def get_locations():
     return random.sample(CELLS, 3)
+
 
 def move_player(player, move):
     x, y = player
@@ -24,7 +37,7 @@ def move_player(player, move):
     if move == "DOWN":
         y += 1
     return x, y
-    
+
 
 def get_moves(player):
     moves = ["LEFT", "RIGHT", "UP", "DOWN"]
@@ -37,27 +50,55 @@ def get_moves(player):
         moves.remove("UP")
     if y == 4:
         moves.remove("DOWN")
-                  
     return moves
 
-monster, door, player = get_location()
 
-while True:
-    valid_moves = get_moves(player)
-    clear_screen()
-    print("Welcome to the dungeon!")
-    print("You're currently in room {}".format(player))#fill with player position
-    print("You can move {}".format(", ".join(get_moves(player)))) # fill with available moves
-    print("Enter QUIT to quit")
+def draw_map(player):
+    print(" _"*5)
+    tile = "|{}"
+    
+    for cell in CELLS:
+        x, y = cell
+        if x < 4:
+            line_end = ""
+            if cell == player:
+                output = tile.format("X")
+            else:
+                output = tile.format("_")
+        else:
+            line_end = "\n"
+            if cell == player:
+                output = tile.format("X|")
+            else:
+                output = tile.format("_|")
+        print(output, end=line_end)
 
-    move = input("> ")
-    move = move.upper()
 
-    if move == 'QUIT':
-        break
-    if move in valid_moves:
-        player = move_player(player, move)
-    else:
-        print("\nCan't move through walls")
-        continue
+def game_loop():
+    monster, door, player = get_locations()
+    
+    while True:
+        draw_map(player)
+        valid_moves = get_moves(player)
+        
+        print("You're currently in room {}".format(player))
+        print("You can move {}".format(", ".join(valid_moves)))
+        print("Enter QUIT to quit")
+        
+        move = input("> ")
+        move = move.upper()
+        
+        if move == 'QUIT':
+            break
+        if move in valid_moves:
+            player = move_player(player, move)
+        else:
+            input("\n ** Walls are hard! Don't run into them! **\n")
+        clear_screen()
 
+
+clear_screen()
+print("Welcome to the dungeon!")
+input("Press return to start!")
+clear_screen()
+game_loop()
